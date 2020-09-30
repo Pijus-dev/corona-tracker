bc83bb4f4c6de4d1dbe2f5a80618ee4fd95568b9
Pijus <user@users-MBP.lan>
2020-09-30 19:11:14 +0300

    Updates

---
+++ precache-manifest.5785f769a7143d94caecf440ebe8c39a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13ca11371c1f705906a8651db8f8e8c",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "16141dd70c26213f6a07",
+    "url": "/corona-tracker/static/css/main.33c1dbfa.chunk.css"
+  },
+  {
+    "revision": "45f995820efd45feca10",
+    "url": "/corona-tracker/static/js/2.b1b74a9f.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/corona-tracker/static/js/2.b1b74a9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16141dd70c26213f6a07",
+    "url": "/corona-tracker/static/js/main.7655a409.chunk.js"
+  },
+  {
+    "revision": "9c4d7b1cf1de7ae99f79",
+    "url": "/corona-tracker/static/js/runtime-main.bf840863.js"
+  },
+  {
+    "revision": "4a8be1ca976664d1aa8fbcb0aa6ebb1d",
+    "url": "/corona-tracker/static/media/logo.4a8be1ca.png"
+  }
+]);
